7f864fcccf9b1b2bb92abfddab60e9d928df6b73
Klautia <klaudia.bahamondez@hotmail.se>
2017-04-12 11:28:57 +0200

    Bekräfta under spelarsidan är där

--- www/js/start-game.js
+++ www/js/start-game.js
@@ -6,7 +6,23 @@ function readStartForm() {
 	let spelare1 = $('#spelare1').val();
 	let spelare2 = $('#spelare2').val();
 
-	$(".page").hide();
-	$("#play").show();
+
+	if(spelare1 === "" || spelare2 === ""){
+		//alert("Skriv in namn på båda spelarna!");
+		$('#varna-ej-namn').modal('show') 
+	}
+	else {
+	    $(".page").hide();
+	    $("#play").show();
+	}
+
+	/*let click = confirm("Press a button");
+	if (click == true) {
+	    x = "You pressed OK!";
+	} else {
+	    x = "You pressed Cancel!";
+	}*/
 
 }
+
+
