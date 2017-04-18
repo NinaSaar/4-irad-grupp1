a37d96b98f5123d2473a416fd95222243fca700b
marialjungberg <k.maria.ljungberg@gmail.com>
2017-04-18 21:28:49 +0200

    Correction of lost code

--- www/js/start-game.js
+++ www/js/start-game.js
@@ -22,6 +22,8 @@ function readStartForm() {
 
 		// Create game object
 	    let game = new Game(player1,player2);
+
+	    document.getElementById("testspelare").innerHTML = spelare1;
 	}
 
 	/*let click = confirm("Press a button");
@@ -33,4 +35,45 @@ function readStartForm() {
 
 }
 
+
+$(function(){
+	$(".col0").click(function(){
+		testFunction(0);
+	});
+});
+$(function(){
+	$(".col1").click(function(){
+		testFunction(1);
+	});
+});
+$(function(){
+	$(".col2").click(function(){
+		testFunction(2);
+	});
+});
+$(function(){
+	$(".col3").click(function(){
+		testFunction(3);
+	});
+});
+$(function(){
+	$(".col4").click(function(){
+		testFunction(4);
+	});
+});
+$(function(){
+	$(".col5").click(function(){
+		testFunction(5);
+	});
+});
+$(function(){
+	$(".col6").click(function(){
+		testFunction(6);
+	});
+});
+
+
+function testFunction(column) {
+	console.log(column);
+}
 
